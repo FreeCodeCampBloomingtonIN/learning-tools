33fe21a3568bb54fa0733abd271090002459b002
Larry Tooley <larry@larrytooley.com>
2016-10-02 16:15:02 -0400

    Add function to read each word

--- js/main.js
+++ js/main.js
@@ -1,5 +1,28 @@
 var synth = window.speechSynthesis;
-var utterThis = new SpeechSynthesisUtterance('This is a test.');
+//var utterThis = new SpeechSynthesisUtterance('This is a test.');
+//synth.speak(utterThis);
+
+sentence = "I eat apples"
+sentenceArr = sentence.split(' ')
+intro = 'The sentence is'
+find = 'Find the word'
+
+
+var intro = new SpeechSynthesisUtterance(intro);
+var utterThis = new SpeechSynthesisUtterance(sentence);
+synth.speak(intro);
 synth.speak(utterThis);
+var findWord = new SpeechSynthesisUtterance(find);
 
 
+// for(var i = 0; i < sentenceArr.length; i++);{
+//     synth.speak(findWord);
+//     var word = new SpeechSynthesisUtterance(sentenceArr[i]);
+//     synth.speak(word);
+// }
+
+for(var i=0; i<sentenceArr.length; i++){
+    synth.speak(findWord);
+    var word = new SpeechSynthesisUtterance(sentenceArr[i]);
+    synth.speak(word);
+}
